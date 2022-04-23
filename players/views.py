8316b95579fb27a9d783a93ceba51ff0648912a3
nuke <rochadcmarcos@gmail.com>
2022-04-23 19:54:09 -0300

    change project to api

--- players/views.py
+++ players/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+
+from .models import Player
+from .serializers import PlayerSerializer
+
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    '''
+    '''
+
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    
