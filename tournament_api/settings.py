81dcccb983cc6f936b57eac07a6fcd9c37698c34
nuke <rochadcmarcos@gmail.com>
2022-04-23 22:59:09 -0300

    sqlite to postgres

--- tournament_api/settings.py
+++ tournament_api/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'tournament_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        
+        'NAME': 'tournament_api_db',
+        
+        'USER': 'tour_user',
+        
+        'PASSWORD': 'tour_db_2021',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432'
     }
 }
+
+
+
 
 
 # Password validation
