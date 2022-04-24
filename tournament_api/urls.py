5db2130eceaf563bea9a05375de4b87a49f2e5a8
nuke <rochadcmarcos@gmail.com>
2022-04-23 22:21:34 -0300

    admin url working

--- tournament_api/urls.py
+++ tournament_api/urls.py
@@ -24,7 +24,7 @@ router = routers.DefaultRouter()
 router.register(r'players', views.PlayerViewSet)
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
