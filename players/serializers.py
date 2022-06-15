d1618a8f04f431f5ef23eea0ee8ac6487b3c4954
Nuke <rochadcmarcos@gmail.com>
2022-06-15 08:54:41 -0300

    add new fields to player

--- players/serializers.py
+++ players/serializers.py
@@ -5,4 +5,5 @@ from .models import Player
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Player 
-        fields = ['id', 'name', 'nationality', 'age', 'overall', 'current_club', 'position', 'matches', 'goals','assists', 'created_at', 'updated_at' ]
+        fields = ['id', 'name', 'nationality', 'age', 'overall', 'current_club', 'position', 'matches', 'goals','assists', 
+        'market_value', 'salary', 'height', 'weight', 'foot','average','created_at', 'updated_at' ]
